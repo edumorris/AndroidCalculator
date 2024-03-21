5461baaa1768dc52347c64cbfd0034f5a555e55f
edumorris <eduuwachira@gmail.com>
2024-03-21 12:41:01 +0300

    Setup calculator layout and input functionality

--- app/src/main/java/com/edumacation/calculator/MainActivity.java
+++ app/src/main/java/com/edumacation/calculator/MainActivity.java
@@ -1,24 +1,70 @@
 package com.edumacation.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText result, newNumber;
+    private TextView displayOperation;
+
+    // variable to hold operands and type of calculations
+    private Double operand1 = null, operand2 = null;
+    private String pendingOp = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        result = (EditText) findViewById(R.id.result);
+        newNumber = (EditText) findViewById(R.id.newNum);
+        displayOperation = (TextView) findViewById(R.id.operation);
+
+        Button btn0 = (Button) findViewById(R.id.btn0);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        Button btn4 = (Button) findViewById(R.id.btn4);
+        Button btn5 = (Button) findViewById(R.id.btn5);
+        Button btn6 = (Button) findViewById(R.id.btn6);
+        Button btn7 = (Button) findViewById(R.id.btn7);
+        Button btn8 = (Button) findViewById(R.id.btn8);
+        Button btn9 = (Button) findViewById(R.id.btn9);
+        Button btnDecimal = (Button) findViewById(R.id.btnDecimal);
+
+        Button btnResult = (Button) findViewById(R.id.btnResult);
+        Button btnDivide = (Button) findViewById(R.id.btnDivide);
+        Button btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        Button btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        Button btnAdd = (Button) findViewById(R.id.btnAdd);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button b = (Button) view;
+                newNumber.append(b.getText().toString());
+            }
+        };
+
+        btn0.setOnClickListener(listener);
+        btn1.setOnClickListener(listener);
+        btn2.setOnClickListener(listener);
+        btn3.setOnClickListener(listener);
+        btn4.setOnClickListener(listener);
+        btn5.setOnClickListener(listener);
+        btn6.setOnClickListener(listener);
+        btn7.setOnClickListener(listener);
+        btn8.setOnClickListener(listener);
+        btn9.setOnClickListener(listener);
+        btnDecimal.setOnClickListener(listener);
+
     }
 }
